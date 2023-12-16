fe8426a6c575e0e32cfb7422391311ff75c35e45
elawadii <elawadym44@example.com>
2023-12-16 19:11:08 +0200

    testing final 5 editing manage eexams page

--- app/src/pages/admin/ManageExamsPage.js
+++ app/src/pages/admin/ManageExamsPage.js
@@ -3,12 +3,18 @@ import ManageExams from '../../components/exams/ManageExams'
 import { Box } from '@mui/material'
 import Header from '../../components/tools/Header'
 import { useSelector } from 'react-redux'
+import { useLocation } from 'react-router-dom'
 
 export default function ManageExamsPage() {
-  const {lang} = useSelector(s => s.global)
+  const { lang, user } = useSelector(s => s.global)
+  const location = useLocation()
+  const isManage = location.pathname.split("/")[1] === "management"
+
   return (
     <Box>
-      <Header title={lang.links.manageExams}/>
+      {user.isAdmin && isManage && (
+        <Header title={lang.links.manageExams} />
+      )}
       <ManageExams />
     </Box>
   )
